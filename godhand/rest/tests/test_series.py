25797406ed5da788d0c70b3bfe1937b522431368
Geoffrey Chan <gc@qsensei.com>
2016-07-25 19:39:06 -0700

    chore: pagination working

--- godhand/rest/tests/test_series.py
+++ godhand/rest/tests/test_series.py
@@ -4,7 +4,7 @@ from .utils import ApiTest
 class TestEmpty(ApiTest):
     def test_create_series(self):
         # retrieve all series
-        expected = {'series': [], 'offset': 0, 'total': 0}
+        expected = {'series': [], 'offset': 0, 'total': 0, 'limit': 10}
         response = self.api.get('/series').json_body
         self.assertEquals(expected, response)
         # create a series
@@ -35,6 +35,7 @@ class TestEmpty(ApiTest):
             }],
             'offset': 0,
             'total': 1,
+            'limit': 10,
         }
         response = self.api.get('/series').json_body
         self.assertEquals(expected, response)
@@ -52,3 +53,30 @@ class TestEmpty(ApiTest):
         }
         response = self.api.get('/series/{}'.format(series_id)).json_body
         self.assertEquals(expected, response)
+        # create more series and try pagination
+        for _ in range(99):
+            response = self.api.post_json(
+                '/series', {
+                    'name': 'Berserk',
+                    'description': 'My Description',
+                    'genres': ['action', 'meme'],
+                    'dbpedia_uri': None,
+                    'author': None,
+                    'magazine': None,
+                    'number_of_volumes': None,
+                }
+            ).json_body
+            self.assertEquals(len(response['series']), 1)
+        response = r0 = self.api.get('/series').json_body
+        self.assertEquals(len(response['series']), 10)
+        self.assertEquals(response['offset'], 0)
+        self.assertEquals(response['limit'], 10)
+        self.assertEquals(response['total'], 100)
+        response = r1 = self.api.get(
+            '/series', params={'offset': 5, 'limit': 2}).json_body
+        self.assertEquals(len(response['series']), 2)
+        self.assertEquals(response['offset'], 5)
+        self.assertEquals(response['limit'], 2)
+        self.assertEquals(response['total'], 100)
+
+        self.assertNotEquals(r0['series'][:2], r1['series'][:2])
--- godhand/rest/utils.py
+++ godhand/rest/utils.py
@@ -5,14 +5,18 @@ class PaginationSchema(co.MappingSchema):
     offset = co.SchemaNode(
         co.Integer(), missing=0, validator=co.Range(min=0),
         location='querystring')
+    limit = co.SchemaNode(
+        co.Integer(), missing=10, validator=co.Range(min=1, max=50),
+        location='querystring')
 
 
 def paginate_query(request, query, key):
     v = request.validated
     db = request.registry['godhand:db']
-    rows = db.query(query, offset=v['offset'])
+    rows = db.query(query, skip=v['offset'], limit=v['limit'])
     return {
         key: [x['key'] for x in rows],
         'offset': rows.offset,
+        'limit': v['limit'],
         'total': rows.total_rows,
     }
